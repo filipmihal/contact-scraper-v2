15849404de37d087a9188a5cd040b112f346a225
Filip Mihal <fifomihal@gmail.com>
2022-08-05 21:49:58 -0700

    Type helper functions

--- contact-scraper-v2/src/helpers.ts
+++ contact-scraper-v2/src/helpers.ts
@@ -21,102 +21,112 @@ function arrayIntersections<T>(arr1: Array<T>, arr2: Array<T>){
     return arr1.filter(value => arr2.includes(value))
 }
 
-// function isSocialEmpty(obj: SocialHandles) {
-//     return Object.keys(obj).every(elem => obj[elem].length === 0)
-// }
+function isSocialEmpty(obj: SocialHandles) {
+    return Object.keys(obj).every(elem => {
+        const socialIndex = elem as keyof SocialHandles
+        return obj[socialIndex].length === 0});
+}
 
-// function getContactObjectLength(obj){
-//     let length = 0;
-//     for (const key in obj) {
-//         if (Object.hasOwnProperty.call(obj, key)) {
-//             length += obj[key].length
-//         }
-//     }
+function getContactObjectLength(obj: SocialHandles){
+    let length = 0;
+    for (const key in obj) {
+        const socialKey = key as keyof SocialHandles;
+        if (Object.hasOwnProperty.call(obj, key)) {
+            length += obj[socialKey].length
+        }
+    }
 
-//     return length
-// }
+    return length
+}
 
-// function jaccardIndex (obj1, obj2){
-//     let unionSize = 0
-//     let intersections = 0
-//     Object.keys(obj1).forEach(elem => {
-//         if(elem !== "phonesUncertain"){
-//             unionSize += arrayUnion(obj1[elem], obj2[elem]).length       
-//             intersections += arrayIntersections(obj1[elem], obj2[elem]).length
-//         }
-//     })
+function jaccardIndex (obj1: SocialHandles, obj2: SocialHandles){
+    let unionSize = 0
+    let intersections = 0
+    Object.keys(obj1).forEach(elem => {
+        const socialKey = elem as keyof SocialHandles;
+        if(elem !== "phonesUncertain"){
+            unionSize += arrayUnion(obj1[socialKey], obj2[socialKey]).length       
+            intersections += arrayIntersections(obj1[socialKey], obj2[socialKey]).length
+        }
+    })
 
-//     return intersections / unionSize
-// }
+    return intersections / unionSize
+}
 
-// function isObjectUnique (obj, savedObjs){
-//     return savedObjs.every(elem => jaccardIndex(obj, elem) < 1)
-//  }
+function isObjectUnique (obj: SocialHandles, savedObjs: SocialHandles[]){
+    return savedObjs.every(elem => jaccardIndex(obj, elem) < 1)
+ }
 
-// function areSocialsTheSame(obj1, obj2){
-//     return jaccardIndex(obj1, obj2) === 1
-// }
+function areSocialsTheSame(obj1: SocialHandles, obj2: SocialHandles){
+    return jaccardIndex(obj1, obj2) === 1
+}
 
-// function buildDuplicityMap(objectList){
-//     const duplicityMap = new Map()
-//     for (const contactObject of objectList) {
-//         for (const key in contactObject) {
-//             if (Object.hasOwnProperty.call(contactObject, key)) {
-//                 contactObject[key].forEach(elem => {
-//                     if(!duplicityMap.get(elem)){
-//                         duplicityMap.set(elem,1)
-//                     }
-//                     else{
-//                         const num = duplicityMap.get(elem)
-//                         duplicityMap.set(elem, num+1)
-//                     }
-//                 })
+function buildDuplicityMap(objectList: SocialHandles[]){
+    const duplicityMap = new Map()
+    for (const contactObject of objectList) {
+        for (const key in contactObject) {
+            if (Object.hasOwnProperty.call(contactObject, key)) {
+                const socialKey = key as keyof SocialHandles;
+                contactObject[socialKey].forEach(elem => {
+                    if(!duplicityMap.get(elem)){
+                        duplicityMap.set(elem,1)
+                    }
+                    else{
+                        const num = duplicityMap.get(elem)
+                        duplicityMap.set(elem, num+1)
+                    }
+                })
                 
-//             }
-//         }
-//     }
+            }
+        }
+    }
 
-//     return duplicityMap
-// }
+    return duplicityMap
+}
 
-// function isContactObjectEdgeCase(contactObject){
-//     // it contains only uncertain phones
-//     for (const key in contactObject) {
-//         if (Object.hasOwnProperty.call(contactObject, key)) {
-//             if(key === 'phonesUncertain')
-//                 continue
-//             else if(contactObject[key].length !== 0)
-//                 return false
-//         }
-//     }
+function isContactObjectEdgeCase(contactObject: SocialHandles){
+    // it contains only uncertain phones
+    for (const key in contactObject) {
+        const socialKey = key as keyof SocialHandles;
+        if (Object.hasOwnProperty.call(contactObject, key)) {
+            if(key === 'phonesUncertain')
+                continue
+            else if(contactObject[socialKey].length !== 0)
+                return false
+        }
+    }
 
-//     return true
-// }
+    return true
+}
 
 
-// function uniqueContactSubsetInheritance(parent, heirs, duplicityMap){
-//     for (const key in parent) {
-//         if (Object.hasOwnProperty.call(parent, key)) {
-//             parent[key].forEach(contactUnit => {
-//                 const unitCount = duplicityMap.get(contactUnit)
-//                 if(unitCount == 1){
-//                     if(heirs.length === 1){
-//                         heirs[0][key].push(contactUnit);
-//                     }
-//                     if(heirs.length > 1){
-//                         console.error("LONELY CONTACT: " + contactUnit)
-//                     }
-//                     // heirs.forEach(heir => heir[key].push(contactUnit))
-//                 }
-//                 else if(unitCount > 1){
-//                     duplicityMap.set(contactUnit, unitCount - 1)
-//                 }
-//                 else{
-//                     throw new Error("Empty Map even though contact exists (Map not synced!!)")
-//                 }
-//             })
+function uniqueContactSubsetInheritance(parent: SocialHandles, heirs: SocialHandles[], duplicityMap: Map<string, number>){
+    for (const key in parent) {
+        if (Object.hasOwnProperty.call(parent, key)) {
+            const socialKey = key as keyof SocialHandles;
+            parent[socialKey].forEach(contactUnit => {
+                const unitCount = duplicityMap.get(contactUnit);
+                if(unitCount === undefined){
+                    throw new Error('Index not found in duplicity map');
+                }
+                if(unitCount == 1){
+                    if(heirs.length === 1){
+                        heirs[0][socialKey].push(contactUnit);
+                    }
+                    if(heirs.length > 1){
+                        console.error("LONELY CONTACT: " + contactUnit)
+                    }
+                    // heirs.forEach(heir => heir[key].push(contactUnit))
+                }
+                else if(unitCount > 1){
+                    duplicityMap.set(contactUnit, unitCount - 1)
+                }
+                else{
+                    throw new Error("Empty Map even though contact exists (Map not synced!!)")
+                }
+            })
             
-//         }
-//     }
+        }
+    }
     
-// }
+}
