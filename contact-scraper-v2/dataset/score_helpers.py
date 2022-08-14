16aaa60b2d2c6fdf3fd5c92b5ac9f89422de686d
Filip Mihal <fifomihal@gmail.com>
2022-08-13 20:05:43 -0700

    New dataset

--- contact-scraper-v2/dataset/score_helpers.py
+++ contact-scraper-v2/dataset/score_helpers.py
@@ -1,13 +1,18 @@
-from dataset_parser import list_union, list_intersection
+from dataset_parser import list_union, list_intersection, levenstein_intersection, levenstein_union
 
 
-def jaccard_index(obj1, obj2, skip_list):
+def jaccard_index(obj1, obj2, skip_list, use_lev=False):
     union_size = 0
     intersections = 0
     for key in obj1.keys():
         if not (key in skip_list):
-            union_size += len(list_union(obj1[key], obj2[key]))
-            intersections += len(list_intersection(obj1[key], obj2[key]))
+            if use_lev:
+                intersections += len(
+                    levenstein_intersection(obj1[key], obj2[key]))
+                union_size += len(levenstein_union(obj1[key], obj2[key]))
+            else:
+                union_size += len(list_union(obj1[key], obj2[key]))
+                intersections += len(list_intersection(obj1[key], obj2[key]))
     if union_size == 0:
         return 1
     return intersections / union_size
